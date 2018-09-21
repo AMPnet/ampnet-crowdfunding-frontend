e810e18a8f4caffe2e5e18e303d61a1d2d34d3f4
Mislav Javor <mislav@ampnet.io>
2018-09-21 13:32:32 +0200

    created withdraw modal

--- src/app/wallet/withdraw-modal/withdraw-modal.component.ts
+++ src/app/wallet/withdraw-modal/withdraw-modal.component.ts
@@ -3,22 +3,21 @@ import { Router } from '@angular/router';
 
 declare var $: any;
 
-
 @Component({
-  selector: 'app-deposit-modal',
-  templateUrl: './deposit-modal.component.html',
-  styleUrls: ['./deposit-modal.component.css']
+  selector: 'app-withdraw-modal',
+  templateUrl: './withdraw-modal.component.html',
+  styleUrls: ['./withdraw-modal.component.css']
 })
-export class DepositModalComponent implements OnInit {
+export class WithdrawModalComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-  ngOnInit() {
+  addPaymentOptionClicked() {
+     $('#withdraw-modal').modal('toggle');
+     this.router.navigate(['dash', 'payment_options', 'new']);
   }
 
-  addPaymentOptionClicked() {
-     $('#deposit-modal').modal('toggle');
-     this.router.navigate(['dash', 'payment_options', 'new']);
+  ngOnInit() {
   }
 
 }

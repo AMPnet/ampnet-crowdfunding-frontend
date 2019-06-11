9074d6c973913081e582efc0c900a5cc2b85cfc4
mjvors <mjvors@mjvorss-MacBook-Pro.local>
2019-06-11 15:47:19 +0200

    update

--- src/app/authentication/onboarding/onboarding.component.ts
+++ src/app/authentication/onboarding/onboarding.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OnboardingService } from '../onboarding.service';
 import swal from 'sweetalert2';
+import { SpinnerUtil } from 'src/app/utilities/spinner-utilities';
 
 declare var initializeIdentyum: any;
 
@@ -19,6 +20,7 @@ export class OnboardingComponent implements OnInit {
   ngOnInit() {
     var that = this;
 
+    SpinnerUtil.showSpinner();
     this.onboardingService.getSessionID().subscribe((res: any) => {
       let identyum = initializeIdentyum({
         webSessionUuid: res.token, // SESSION_ID from POST request
@@ -27,6 +29,9 @@ export class OnboardingComponent implements OnInit {
           if(args.status == "success") {
             that.router.navigateByUrl("/dash");
           }
+        },
+        onWidgetInitialized: function(args) {
+          SpinnerUtil.hideSpinner();
         } 
       });
       identyum.load();

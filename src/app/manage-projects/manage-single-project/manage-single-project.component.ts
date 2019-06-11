597324ea9f97189074f4489687c0fd0866dacc86
mjvors <mjvors@mjvorss-MacBook-Pro.local>
2019-06-11 17:29:48 +0200

    added uppy uploading to app

--- src/app/manage-projects/manage-single-project/manage-single-project.component.ts
+++ src/app/manage-projects/manage-single-project/manage-single-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Uppy from 'uppy';
+import { XHRUpload, DragDrop } from 'uppy'; 
 
 @Component({
   selector: 'app-manage-single-project',
@@ -10,6 +12,21 @@ export class ManageSingleProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    var uppy = Uppy.Core({
+      restrictions: {
+        maxFileSize: null,
+        maxNumberOfFiles: 1,
+        minNumberOfFiles: 1,
+        allowedFileTypes: ["image/*"]
+      }
+    });
+    uppy.use(Uppy.Dropbox, { });
+    uppy.use(Uppy.Tus, { endpoint: 'https://master.tus.io/files/' })
+    uppy.use(Uppy.Dashboard, { 
+      target: '#drag-drop-area',
+      inline: true
+    });
   }
 
 }

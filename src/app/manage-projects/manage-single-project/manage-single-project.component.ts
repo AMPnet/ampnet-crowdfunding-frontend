6d3077764c0261653a55554b4ab794c0790b1728
mjvors <mjvors@mjvorss-MacBook-Pro.local>
2019-06-14 14:15:02 +0200

    added upload areas to single project management

--- src/app/manage-projects/manage-single-project/manage-single-project.component.ts
+++ src/app/manage-projects/manage-single-project/manage-single-project.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as Uppy from 'uppy';
-import { XHRUpload, DragDrop } from 'uppy'; 
+import { XHRUpload, DragDrop, Dashboard } from 'uppy'; 
+
+declare var $: any;
 
 @Component({
   selector: 'app-manage-single-project',
@@ -12,21 +14,44 @@ export class ManageSingleProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setUploadAreas();
 
-    var uppy = Uppy.Core({
+  }
+
+  private setUpUppy(id: string, allowedFileTypes: string[]): Uppy.Core.Uppy {
+    return Uppy.Core({
+      id: id,
       restrictions: {
         maxFileSize: null,
         maxNumberOfFiles: 1,
         minNumberOfFiles: 1,
-        allowedFileTypes: ["image/*"]
+        allowedFileTypes: allowedFileTypes
       }
     });
-    uppy.use(Uppy.Dropbox, { });
-    uppy.use(Uppy.Tus, { endpoint: 'https://master.tus.io/files/' })
+  } 
+
+  private configureUppy(uppy: Uppy.Core.Uppy , areaID: string) {
+    let width = $("#core-root-manager").width();
     uppy.use(Uppy.Dashboard, { 
-      target: '#drag-drop-area',
-      inline: true
+      target: areaID,
+      inline: true,
+      height: 300,
+      width: width
     });
+    uppy.use(Uppy.Tus, { endpoint: 'https://master.tus.io/files/' })
+  }
+  
+  private setUploadAreas() {
+
+    var imageUppy = this.setUpUppy("image-upload-project", ["image/*"],)
+    this.configureUppy(imageUppy, "#drag-drop-area-img");
+
+    var filesUppy = this.setUpUppy("file-upload-project", ["*"])
+    this.configureUppy(filesUppy, "#drag-drop-area-files");
+  }
+
+  private setUploadFilesProject() {
+
   }
 
 }

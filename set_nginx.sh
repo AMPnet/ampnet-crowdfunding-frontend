ae12f6d49b308e78347183fb7166b89bbbfd4973
mjvrs <mjvrs@mjvrss-MacBook-Pro.local>
2019-08-28 16:15:06 +0200

    updateE

--- set_nginx.sh
+++ set_nginx.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Passing new Nginx configuration to server \n" &&
 echo 'k9B3aaeF' | scp ./nginx.conf root@ampnet.io:/etc/nginx/nginx.conf &&
-echo 'k9B3aaeF' | ssh root@ampnet.io 'nginx -s reload'
+echo "Successfully passed new nginx configuration \n" &&
+echo "Reloading nginx \n" &&
+echo 'k9B3aaeF' | ssh root@ampnet.io 'nginx -s reload' &&
+echo "Successfully reloaded new config \n"

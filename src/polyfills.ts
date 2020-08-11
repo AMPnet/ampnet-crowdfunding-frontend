ea66985a3f801944a6b906361146d9c5b17af527
Matija Pevec <pevec@live.com>
2020-08-11 10:49:44 +0200

    fixed issue with process polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -69,6 +69,8 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
 
+import * as process from 'process';
+window['process'] = process;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS

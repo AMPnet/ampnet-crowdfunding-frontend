47b7d66b00fef1ed89a32e2fbb4a4aa1e78353c2
Marko <marko@ampnet.io>
2020-08-14 13:33:36 +0200

    Added custom currency pipe

---
+++ src/app/pipes/currency-default-pipe.ts
@@ -0,0 +1,21 @@
+import { CurrencyPipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'currencyDefault'
+})
+export class CurrencyDefaultPipe extends CurrencyPipe implements PipeTransform {
+    constructor() {
+        super('en');
+    }
+
+    transform(
+        value: any,
+        code = 'EUR',
+        display = 'symbol',
+        digitsInfo = '1.2-2',
+        locale = 'en'
+    ) {
+        return super.transform(value, code, display, digitsInfo, locale);
+    }
+}

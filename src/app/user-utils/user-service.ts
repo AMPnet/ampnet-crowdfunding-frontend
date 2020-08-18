a3b3600dec4c1d0abe32c1cf6abac28dd50bdc07
Marko <shark.marko@gmail.com>
2020-08-18 22:20:40 +0200

    Added refreshUserToken function

--- src/app/user-utils/user-service.ts
+++ src/app/user-utils/user-service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { UserStatusStorage } from '../user-status-storage';
 import { tap } from 'rxjs/operators';
 import { UserModel } from '../models/user-model';
+import { TokenModel } from '../models/auth/TokenModel';
 
 @Injectable({
     providedIn: 'root'
@@ -20,6 +21,16 @@ export class UserService {
             .pipe(
                 tap(res => {
                     UserStatusStorage.personalData = res;
+                }));
+    }
+
+    refreshUserToken() {
+        return this.http.post<TokenModel>(API.generateRoute('/user/token/refresh'),
+            {'refresh_token': localStorage.getItem('refresh_token')}, API.tokenHeaders())
+            .pipe(
+                tap(data => {
+                    localStorage.setItem('access_token', data.access_token);
+                    localStorage.setItem('refresh_token', data.refresh_token);
                 }));
     }
 }

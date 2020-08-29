7d7a7e0f74e20366edea8fd347b5b585f5a43afe
Marko <shark.marko@gmail.com>
2020-08-29 23:44:10 +0200

    Close dialog fix

--- src/app/project/manage-payments/revenue-share-confirm-modal/revenue-share-confirm-modal.component.ts
+++ src/app/project/manage-payments/revenue-share-confirm-modal/revenue-share-confirm-modal.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs';
 import { autonumericCurrency } from '../../../utilities/currency-util';
 
 @Component({
     selector: 'app-revenue-share-confirm-modal',
     templateUrl: './revenue-share-confirm-modal.component.html',
-    styleUrls: ['./revenue-share-confirm-modal.component.css'],
+    styleUrls: ['./revenue-share-confirm-modal.component.css']
 })
 
 export class RevenueShareConfirmModalComponent implements OnInit {

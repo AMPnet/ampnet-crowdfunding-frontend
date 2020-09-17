99cc38f5e3491565caf444d06d51acf486cd758b
Marko <marko@ampnet.io>
2020-09-17 11:28:41 +0200

    Fixed deposit flow

--- src/app/manage-deposits/manage-single-deposit/manage-single-deposit.component.ts
+++ src/app/manage-deposits/manage-single-deposit/manage-single-deposit.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import * as Uppy from 'uppy';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { hideSpinnerAndDisplayError } from 'src/app/utilities/error-handler';
 import { SpinnerUtil } from 'src/app/utilities/spinner-utilities';
 import * as numeral from 'numeral';
@@ -11,12 +11,10 @@ import {
 import { ArkaneConnect, SecretType, SignatureRequestType, WindowMode } from '@arkane-network/arkane-connect';
 import { BroadcastService } from 'src/app/shared/services/broadcast.service';
 import swal from 'sweetalert2';
-import { autonumericCurrency, baseCurrencyUnitToCents } from 'src/app/utilities/currency-util';
-import MicroModal from 'micromodal';
+import { autonumericCurrency, baseCurrencyUnitToCents, stripCurrencyData } from 'src/app/utilities/currency-util';
 import { BackendHttpClient } from '../../shared/services/backend-http-client.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ManageSingleDepositModalComponent } from './manage-single-deposit-modal/manage-single-deposit-modal.component';
-import { KeyboardType } from 'tns-core-modules/ui/enums';
 
 declare var $: any;
 
@@ -25,7 +23,7 @@ declare var $: any;
     templateUrl: './manage-single-deposit.component.html',
     styleUrls: ['./manage-single-deposit.component.css']
 })
-export class ManageSingleDepositComponent implements OnInit {
+export class ManageSingleDepositComponent implements OnInit, AfterViewInit {
     @Input() depositAmount;
     confirmationModal: BsModalRef;
     depositModel: DepositSearchResponse;
@@ -35,15 +33,15 @@ export class ManageSingleDepositComponent implements OnInit {
                 private http: BackendHttpClient,
                 private depositCooperativeService: WalletCooperativeDepositService,
                 private broadService: BroadcastService,
-                private modalService: BsModalService) {
+                private modalService: BsModalService,
+                private router: Router) {
     }
 
     ngOnInit() {
         this.getDeposit();
     }
 
-
-    createUploadArea() {
+    ngAfterViewInit(): void {
         this.paymentUppy = Uppy.Core();
 
         this.paymentUppy.use(Uppy.Dashboard, {
@@ -55,13 +53,13 @@ export class ManageSingleDepositComponent implements OnInit {
             note: 'Upload payment reciept for deposit',
             hideUploadButton: true,
         });
+        autonumericCurrency('#deposit-amount');
     }
 
     generateSignerAndSign() {
         SpinnerUtil.showSpinner();
 
         this.depositCooperativeService.generateDepositMintTx(this.depositModel.deposit.id).subscribe(async res => {
-            SpinnerUtil.hideSpinner();
             const arkaneConnect = new ArkaneConnect('AMPnet', {
                 environment: 'staging'
             });
@@ -76,32 +74,23 @@ export class ManageSingleDepositComponent implements OnInit {
             this.broadService.broadcastSignedTx(sigRes.result.signedTransaction, res.tx_id)
                 .subscribe(_ => {
                     SpinnerUtil.hideSpinner();
-                    swal('', 'Success', 'success');
+                    swal('', 'Success', 'success')
+                        .then(() => this.router.navigate(['/dash/manage_deposits']));
                 }, hideSpinnerAndDisplayError);
         }, hideSpinnerAndDisplayError);
     }
 
     getDeposit() {
-        const id = this.route.snapshot.params.ID;
         SpinnerUtil.showSpinner();
-        this.depositCooperativeService.getDeposit(id).subscribe(res => {
-            this.depositModel = res;
 
+        const id = this.route.snapshot.params.ID;
+        this.depositCooperativeService.getDeposit(id).subscribe(res => {
+            SpinnerUtil.hideSpinner();
+            this.depositModel = res;
             this.depositModel.deposit.amount = numeral(this.depositModel.deposit.amount).format(',');
-            if (!this.depositModel.deposit.approved) {
-                setTimeout(() => {
-                    this.createUploadArea();
-                    MicroModal.init();
-                }, 300);
-            } else {
+            if (this.depositModel.deposit.approved) {
                 this.generateSignerAndSign();
             }
-
-            setTimeout(() => {
-                autonumericCurrency('#deposit-amount');
-            }, 200);
-
-            SpinnerUtil.hideSpinner();
         }, hideSpinnerAndDisplayError);
     }
 
@@ -113,11 +102,10 @@ export class ManageSingleDepositComponent implements OnInit {
         });
         const confirmationSub = this.confirmationModal.content.onSuccessfulConfirmation
             .subscribe(() => {
-                console.log('subscribe');
                 const depositApprovalURL = this.depositCooperativeService.generateDepositApprovalURL(
                     location.origin,
                     this.depositModel.deposit.id,
-                    Number(baseCurrencyUnitToCents(this.depositAmount))
+                    baseCurrencyUnitToCents(Number(stripCurrencyData(this.depositAmount)))
                 );
 
                 this.paymentUppy.use(Uppy.XHRUpload, {
@@ -128,11 +116,9 @@ export class ManageSingleDepositComponent implements OnInit {
                     }
                 });
                 confirmationSub.unsubscribe();
-                // SpinnerUtil.showSpinner();
-                // this.paymentUppy.upload().then(() => {
-                //     SpinnerUtil.hideSpinner();
-                //     this.getDeposit();
-                // });
-            });
+                this.paymentUppy.upload().then(() => {
+                    this.getDeposit();
+                }, hideSpinnerAndDisplayError);
+            }, hideSpinnerAndDisplayError);
     }
 }

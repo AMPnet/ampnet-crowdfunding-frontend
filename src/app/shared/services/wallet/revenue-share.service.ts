8b993843c0ce70ebeccf4ee7d104ba9f923495d2
Matija Pevec <pevec@live.com>
2020-09-30 11:00:14 +0200

    removed unnecessary import

--- src/app/shared/services/wallet/revenue-share.service.ts
+++ src/app/shared/services/wallet/revenue-share.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BackendHttpClient } from '../backend-http-client.service';
 import { TransactionInfo } from './wallet-cooperative/wallet-cooperative-wallet.service';
-import { baseCurrencyUnitToCents, centsToBaseCurrencyUnit } from '../../../utilities/currency-util';
 
 @Injectable({
     providedIn: 'root'

8d1a6d5a7ee3eda1a6c10672dda86bcbf61fd9b0
Matija Pevec <pevec@live.com>
2020-10-12 18:54:19 +0200

    modified input map input data to fit angular forms

--- src/app/manage-projects/manage-single-project/manage-single-project.component.ts
+++ src/app/manage-projects/manage-single-project/manage-single-project.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as Uppy from 'uppy';
 import { ActivatedRoute, Router } from '@angular/router';
 import { displayBackendErrorRx } from 'src/app/utilities/error-handler';
@@ -9,10 +9,11 @@ import { Project, ProjectService } from '../../shared/services/project/project.s
 import { WalletService } from '../../shared/services/wallet/wallet.service';
 import { BackendHttpClient } from '../../shared/services/backend-http-client.service';
 import { ManageProjectsService } from '../../shared/services/project/manage-projects.service';
-import { catchError, finalize, switchMap, tap } from 'rxjs/operators';
-import { EMPTY, throwError } from 'rxjs';
+import { catchError, finalize, map, switchMap, tap } from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, Observable, of, throwError } from 'rxjs';
 import { ArkaneService } from '../../shared/services/arkane.service';
 import { PopupService } from '../../shared/services/popup.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 declare var $: any;
 
@@ -21,20 +22,13 @@ declare var $: any;
     templateUrl: './manage-single-project.component.html',
     styleUrls: ['./manage-single-project.component.css'],
 })
-export class ManageSingleProjectComponent implements OnInit, AfterViewInit {
+export class ManageSingleProjectComponent implements OnInit {
+    project$: Observable<Project>;
+    projectWallet$: Observable<WalletDetails>;
+    updateForm$: Observable<FormGroup>;
 
-    public files: string[] = [
-        'Building permit',
-        'Working permit',
-        'Ecological permit',
-        'Development certificate'
-    ];
-    project: Project;
-    wallet: WalletDetails;
-    qrCodeData: String = '';
-    mapLat: number;
-    mapLong: number;
-    projectCoords = [];
+    refreshProjectSubject = new BehaviorSubject<Project>(null);
+    refreshProjectWalletSubject = new BehaviorSubject<void>(null);
 
     constructor(private projectService: ProjectService,
                 private walletService: WalletService,
@@ -43,48 +37,81 @@ export class ManageSingleProjectComponent implements OnInit, AfterViewInit {
                 private arkaneService: ArkaneService,
                 private popupService: PopupService,
                 private router: Router,
+                private fb: FormBuilder,
                 private route: ActivatedRoute) {
-    }
+        const projectUUID = this.route.snapshot.params.projectID;
+        this.project$ = this.refreshProjectSubject.pipe(
+            switchMap(project => project !== null ? of(project) : this.projectService.getProject(projectUUID)),
+        );
 
-    ngOnInit() {
-        this.fetchAllData().subscribe();
-    }
-
-    ngAfterViewInit() {
-    }
-
-    fetchAllData() {
-        SpinnerUtil.showSpinner();
-        return this.getProject().pipe(
-            switchMap(() => this.walletService.getProjectWallet(this.project.uuid)),
+        this.projectWallet$ = this.refreshProjectWalletSubject.pipe(
+            switchMap(() => this.walletService.getProjectWallet(projectUUID)),
             catchError(err => {
                 if (err.status === 404) {
-                    this.createProjectWallet().subscribe();
+                    this.createProjectWallet(projectUUID).subscribe();
                     return EMPTY;
                 } else {
-                    throwError(err);
+                    return throwError(err);
                 }
             }),
             displayBackendErrorRx(),
-            tap(res => {
-                this.wallet = res;
+        );
 
-                setTimeout(function () {
-                    this.setUploadAreas();
-                }.bind(this));
-            }),
-            finalize(() => SpinnerUtil.hideSpinner())
+        this.updateForm$ = this.project$.pipe(map(project => {
+                return fb.group({
+                    name: [project.name, Validators.required],
+                    description: [project.description, Validators.minLength(3)],
+                    location: fb.group({
+                        lat: [project.location.lat],
+                        long: [project.location.long],
+                    }),
+                    roi: fb.group({
+                        from: [project.roi.from],
+                        to: [project.roi.to],
+                    }),
+                    image: [null],
+                    documents: [[]]
+                });
+            })
         );
     }
 
-    createProjectWallet() {
+    ngOnInit() {
+        // this.fetchAllData().subscribe();
+    }
+
+    // fetchAllData() {
+    //     SpinnerUtil.showSpinner();
+    //     return this.getProject().pipe(
+    //         switchMap(() => this.walletService.getProjectWallet(this.project.uuid)),
+    //         catchError(err => {
+    //             if (err.status === 404) {
+    //                 this.createProjectWallet().subscribe();
+    //                 return EMPTY;
+    //             } else {
+    //                 throwError(err);
+    //             }
+    //         }),
+    //         displayBackendErrorRx(),
+    //         tap(res => {
+    //             this.wallet = res;
+    //
+    //             setTimeout(function () {
+    //                 this.setUploadAreas();
+    //             }.bind(this));
+    //         }),
+    //         finalize(() => SpinnerUtil.hideSpinner())
+    //     );
+    // }
+
+    createProjectWallet(projectUUID: string) {
         return this.popupService.new({
             type: 'info',
             text: 'Verify the project creation with your blockchain wallet. You will be prompted now!'
         }).pipe(
             tap(() => SpinnerUtil.showSpinner()),
             switchMap(() =>
-                this.walletService.createProjectWalletTransaction(this.project.uuid)
+                this.walletService.createProjectWalletTransaction(projectUUID)
                     .pipe(displayBackendErrorRx())),
             switchMap(txInfo => this.arkaneService.signAndBroadcastTx(txInfo).pipe(
                 catchError(_ => {
@@ -97,18 +124,21 @@ export class ManageSingleProjectComponent implements OnInit, AfterViewInit {
                 title: 'Transaction signed',
                 text: 'Transaction is being processed...'
             })),
-            switchMap(() => this.fetchAllData()),
+            tap(() => {
+                this.refreshProjectSubject.next(null);
+                this.refreshProjectWalletSubject.next();
+            }),
             finalize(() => SpinnerUtil.hideSpinner())
         );
     }
 
-    addNewsLink() {
+    addNewsLink(project: Project) {
         const linkHolder = $('#newsLink').val();
         if (validURL(linkHolder)) {
-            return this.manageProjectsService.addNewsToProject(this.project, linkHolder).pipe(
+            return this.manageProjectsService.addNewsToProject(project, linkHolder).pipe(
                 displayBackendErrorRx(),
                 tap(updatedProject => {
-                    this.project = updatedProject;
+                    this.refreshProjectSubject.next(updatedProject);
                     $('#newsLink').val('');
                 })
             );
@@ -120,68 +150,61 @@ export class ManageSingleProjectComponent implements OnInit, AfterViewInit {
         }
     }
 
-    deleteNewsClicked(link: string) {
+    deleteNewsClicked(project: Project, link: string) {
         SpinnerUtil.showSpinner();
-        this.manageProjectsService.deleteNewsFromProject(this.project, link).pipe(
+        this.manageProjectsService.deleteNewsFromProject(project, link).pipe(
             displayBackendErrorRx(),
-            switchMap(() => this.getProject()),
+            tap(() => this.refreshProjectSubject.next(null)),
             finalize(() => SpinnerUtil.hideSpinner())
         );
     }
 
-    getProject() {
-        const id = this.route.snapshot.params.projectID;
-        return this.projectService.getProject(id).pipe(
+    // getProject() {
+    //     const id = this.route.snapshot.params.projectID;
+    //     return this.projectService.getProject(id).pipe(
+    //         displayBackendErrorRx(),
+    //         tap(res => {
+    //             this.project = res;
+    //             this.mapLat = this.project.location.lat;
+    //             this.mapLong = this.project.location.long;
+    //         })
+    //     );
+    // }
+
+    toggleProjectStatusClicked(project: Project) {
+        return this.projectService.updateProject(project.uuid, {active: !project.active}).pipe(
             displayBackendErrorRx(),
-            tap(res => {
-                this.project = res;
-                this.mapLat = this.project.location.lat;
-                this.mapLong = this.project.location.long;
-            })
+            tap(() => this.refreshProjectSubject.next(null)),
         );
     }
 
-    toggleProjectStatusClicked() {
-        return this.projectService.updateProject(this.project.uuid, {active: !this.project.active}).pipe(
-            displayBackendErrorRx(),
-            switchMap(() => this.getProject()),
-        );
+    updateProject(project: Project, form: FormGroup) {
+        return () => {
+            const controls = form.controls;
+            console.log(controls['location'].value);
+            return this.projectService.updateProject(project.uuid, {
+                name: controls['name'].value,
+                description: controls['description'].value,
+                location: controls['location'].value,
+                roi: controls['roi'].value,
+            }).pipe(
+                displayBackendErrorRx(),
+                tap(updatedProject => this.refreshProjectSubject.next(updatedProject)),
+            );
+        };
     }
 
-    updateProject() {
-        const projectName = $('#project-name').val();
-        const projectDescription = $('#project-description').val();
-        const locationName = $('#location-name').val();
-
-        const updatedProject = this.project;
-        updatedProject.name = projectName;
-        updatedProject.description = projectDescription;
-        updatedProject.location_text = locationName;
-        updatedProject.location = {lat: this.mapLat, long: this.mapLong};
-
-        return this.projectService.updateProject(updatedProject.uuid, {
-            name: updatedProject.name,
-            description: updatedProject.description,
-            location: updatedProject.location,
-            roi: updatedProject.roi,
-            active: updatedProject.active
-        }).pipe(
-            displayBackendErrorRx(),
-            switchMap(() => this.getProject()),
-        );
-    }
-
-    deleteFile(index: number): any {
+    deleteFile(project: Project, documentID: number) {
         return this.popupService.new({
             text: 'Are you sure you want to delete this file? This action cannot be reversed',
             confirmButtonText: 'Yes',
             showCancelButton: true,
             cancelButtonText: 'No'
         }).pipe(
+            switchMap(res => res.value === true ?
+                this.manageProjectsService.deleteDocument(project.uuid, documentID).pipe(displayBackendErrorRx()) : EMPTY),
             tap(() => SpinnerUtil.showSpinner()),
-            switchMap(() => this.manageProjectsService.deleteDocument(this.project.uuid, index)
-                .pipe(displayBackendErrorRx())),
-            switchMap(() => this.getProject()),
+            tap(() => this.refreshProjectSubject.next(null)),
             finalize(() => SpinnerUtil.hideSpinner())
         );
     }
@@ -198,57 +221,57 @@ export class ManageSingleProjectComponent implements OnInit, AfterViewInit {
         });
     }
 
-    private configureUppy(uppy: Uppy.Core.Uppy, areaID: string) {
-        const width = $('#core-root-manager').width();
-        uppy.use(Uppy.Dashboard, {
-            target: areaID,
-            inline: true,
-            height: 300,
-            width: width
-        });
+    // private configureUppy(uppy: Uppy.Core.Uppy, areaID: string) {
+    //     const width = $('#core-root-manager').width();
+    //     uppy.use(Uppy.Dashboard, {
+    //         target: areaID,
+    //         inline: true,
+    //         height: 300,
+    //         width: width
+    //     });
+    //
+    //     uppy.use(Uppy.XHRUpload, {
+    //         endpoint: `/api/project/project/${this.project.uuid}/image/main`,
+    //         headers: {
+    //             'Authorization': this.http.authHttpOptions().headers.get('Authorization')
+    //         },
+    //         metaFields: [
+    //             'name', 'filename'
+    //         ],
+    //         fieldName: 'image'
+    //     }).on('upload-success', () => {
+    //         this.fetchAllData().subscribe();
+    //     });
+    // }
 
-        uppy.use(Uppy.XHRUpload, {
-            endpoint: `/api/project/project/${this.project.uuid}/image/main`,
-            headers: {
-                'Authorization': this.http.authHttpOptions().headers.get('Authorization')
-            },
-            metaFields: [
-                'name', 'filename'
-            ],
-            fieldName: 'image'
-        }).on('upload-success', () => {
-            this.fetchAllData().subscribe();
-        });
-    }
-
-    private setUploadAreas() {
-        const imageUppy = this.setUpUppy('image-upload-project', ['image/*']);
-        this.configureUppy(imageUppy, '#drag-drop-area-img');
-
-        const filesUppy = Uppy.Core({
-            id: 'files-upload-uppy'
-        });
-        const width = $('#core-root-manager').width();
-        filesUppy.use(Uppy.Dashboard, {
-            target: '#files-upload-uppy',
-            inline: true,
-            height: 200,
-            width: width
-        });
-
-        filesUppy.use(Uppy.XHRUpload, {
-            endpoint: `/api/project/project/${this.project.uuid}/document`,
-            headers: {
-                'Authorization': this.http.authHttpOptions().headers.get('Authorization')
-            },
-            metaFields: [
-                'name', 'filename'
-            ],
-            fieldName: 'file',
-            bundle: false
-        }).on('upload-success', () => {
-            this.fetchAllData().subscribe();
-            filesUppy.close();
-        });
-    }
+    // private setUploadAreas() {
+    //     const imageUppy = this.setUpUppy('image-upload-project', ['image/*']);
+    //     this.configureUppy(imageUppy, '#drag-drop-area-img');
+    //
+    //     const filesUppy = Uppy.Core({
+    //         id: 'files-upload-uppy'
+    //     });
+    //     const width = $('#core-root-manager').width();
+    //     filesUppy.use(Uppy.Dashboard, {
+    //         target: '#files-upload-uppy',
+    //         inline: true,
+    //         height: 200,
+    //         width: width
+    //     });
+    //
+    //     filesUppy.use(Uppy.XHRUpload, {
+    //         endpoint: `/api/project/project/${this.project.uuid}/document`,
+    //         headers: {
+    //             'Authorization': this.http.authHttpOptions().headers.get('Authorization')
+    //         },
+    //         metaFields: [
+    //             'name', 'filename'
+    //         ],
+    //         fieldName: 'file',
+    //         bundle: false
+    //     }).on('upload-success', () => {
+    //         this.fetchAllData().subscribe();
+    //         filesUppy.close();
+    //     });
+    // }
 }

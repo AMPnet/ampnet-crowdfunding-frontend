648c946c69910dbe08951000de65f3618b02b655
Matija Pevec <pevec@live.com>
2020-11-02 12:21:42 +0100

    fixed empty lines

--- src/app/public-layout/header/header.component.ts
+++ src/app/public-layout/header/header.component.ts
@@ -1,14 +1,12 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { AppConfigService } from '../../shared/services/app-config.service';
 
-
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
-
     @Input() onlyLogo = false;
 
     constructor(public appConfigService: AppConfigService) {
@@ -18,7 +16,5 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
-
     }
-
 }

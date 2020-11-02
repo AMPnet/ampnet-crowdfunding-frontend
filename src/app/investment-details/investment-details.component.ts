b04fac00881ea9c6580311f7f3a88eaf02bb6219
Matija Pevec <pevec@live.com>
2020-11-02 16:17:04 +0100

    fixed issue with fetching data; refactoring offer details component

--- src/app/investment-details/investment-details.component.ts
+++ src/app/investment-details/investment-details.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { InvestmentsInProject, PortfolioService } from '../shared/services/wallet/portfolio.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { displayBackendErrorRx, hideSpinnerAndDisplayError } from '../utilities/error-handler';
-import { SpinnerUtil } from '../utilities/spinner-utilities';
-import { Observable } from 'rxjs';
-import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-investment-details',
@@ -12,23 +6,6 @@ import { map, shareReplay, switchMap } from 'rxjs/operators';
     styleUrls: ['./investment-details.component.scss']
 })
 export class InvestmentDetailsComponent {
-    investment$: Observable<InvestmentsInProject>;
-    isCancelable$: Observable<boolean>;
-
-    constructor(private portfolioService: PortfolioService,
-                private activatedRoute: ActivatedRoute) {
-        const id = this.activatedRoute.snapshot.params.id;
-        this.investment$ = this.portfolioService.getInvestmentsInProject(id)
-            .pipe(shareReplay(1));
-
-        this.isCancelable$ = this.investment$.pipe(
-            switchMap(investment => {
-                const transaction = investment.transactions[0];
-                return this.portfolioService.isInvestmentCancelable(transaction.to_tx_hash, transaction.from_tx_hash).pipe(
-                    displayBackendErrorRx(),
-                    map(res => res.can_cancel)
-                );
-            })
-        );
+    constructor() {
     }
 }

3b9ac1fd842888ba535d3b7279489ae6c56dc619
Marko <marko@ampnet.io>
2020-11-04 12:51:53 +0100

    Change Event Emitter name

--- src/app/navbar/directives/click-outside.directive.ts
+++ src/app/navbar/directives/click-outside.directive.ts
@@ -8,13 +8,13 @@ export class ClickOutsideDirective {
     }
 
     @Output()
-    public clickOutside = new EventEmitter();
+    public clickOutsideSidebar = new EventEmitter();
 
     @HostListener('document:click', ['$event.target'])
     public onClick(targetElement) {
         const clickedInside = this.elementRef.nativeElement.contains(targetElement);
         if (!clickedInside) {
-            this.clickOutside.emit(null);
+            this.clickOutsideSidebar.emit(null);
         }
     }
 }

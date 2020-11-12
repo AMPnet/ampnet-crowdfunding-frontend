d73fa31eebec9cb4ff2b0e278985d9da616c5c8c
Karlo Skafec <karlo@MacBook-Pro.local>
2020-11-12 16:28:59 +0100

    fixes

--- src/app/organizations/organization-details/manage-projects/manage-projects.component.ts
+++ src/app/organizations/organization-details/manage-projects/manage-projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { OrganizationService } from '../../../shared/services/project/organization.service';
 import { displayBackendErrorRx } from '../../../utilities/error-handler';

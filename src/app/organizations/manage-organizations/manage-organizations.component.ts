6f58d4ae8733f5c9fa3862326441545a3dcbdf48
Karlo Skafec <karlo@MacBook-Pro.local>
2020-11-19 15:04:48 +0100

    alerts fixed

--- src/app/organizations/manage-organizations/manage-organizations.component.ts
+++ src/app/organizations/manage-organizations/manage-organizations.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Organization, OrganizationInvite, OrganizationService } from '../../shared/services/project/organization.service';
 import { displayBackendErrorRx } from 'src/app/utilities/error-handler';
 import { SpinnerUtil } from 'src/app/utilities/spinner-utilities';
@@ -11,7 +11,7 @@ import { PopupService } from '../../shared/services/popup.service';
     templateUrl: './manage-organizations.component.html',
     styleUrls: ['./manage-organizations.component.scss']
 })
-export class ManageOrganizationsComponent {
+export class ManageOrganizationsComponent implements OnInit {
     organizations$: Observable<Organization[]>;
     organizationInvites$: Observable<OrganizationInvite[]>;
 
@@ -48,5 +48,11 @@ export class ManageOrganizationsComponent {
             tap(() => this.refreshState()),
             finalize(() => SpinnerUtil.hideSpinner())
         ).subscribe();
+    }
+
+    ngOnInit() {
+        /* this.popupService.new({
+            type: 'error', text: 'Accepted invitation to organization'
+        }); */
     }
 }

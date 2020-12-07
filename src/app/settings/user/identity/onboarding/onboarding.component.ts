7040a40311442022db0d722021a451033c9c65a2
Matija Pevec <pevec@live.com>
2020-12-07 15:21:26 +0100

    wip veriff flow

--- src/app/settings/user/identity/onboarding/onboarding.component.ts
+++ src/app/settings/user/identity/onboarding/onboarding.component.ts
@@ -1,13 +1,17 @@
 import { Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
 import { OnboardingService } from '../../../../shared/services/user/onboarding.service';
-import { displayBackendErrorRx } from 'src/app/utilities/error-handler';
 import { UserAuthService } from '../../../../shared/services/user/user-auth.service';
 import { DOCUMENT } from '@angular/common';
 import { combineLatest, EMPTY, Observable, Subject } from 'rxjs';
-import { catchError, switchMap, take } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 import { PopupService } from '../../../../shared/services/popup.service';
 import { AppConfigService } from '../../../../shared/services/app-config.service';
 import { RouterService } from '../../../../shared/services/router.service';
+import { UserService } from '../../../../shared/services/user/user.service';
+import { createVeriffFrame, MESSAGES } from '@veriff/incontext-sdk';
+import { User } from '../../../../shared/services/user/signup.service';
+import Veriff from '@veriff/js-sdk';
+
 
 @Component({
     selector: 'app-onboarding',
@@ -19,80 +23,109 @@ export class OnboardingComponent {
 
     loaded$: Observable<void>;
     finished$: Observable<void>;
-    finishedSubject: Subject<IdentyumClientToken>;
+    finishedSubject = new Subject<VeriffFormResponse>();
 
     constructor(private renderer2: Renderer2,
                 @Inject(DOCUMENT) private document: Document,
                 private appConfig: AppConfigService,
                 private router: RouterService,
                 private popupService: PopupService,
+                private userService: UserService,
                 private onboardingService: OnboardingService,
                 private loginService: UserAuthService) {
-        this.loaded$ = combineLatest([this.onboardingService.getSessionID(), this.loadIdentyumScript()]).pipe(
-            take(1),
-            switchMap(([clientToken, _]) => this.setFlowManager(clientToken, this.appConfig.config.config.identyum.startLanguage))
+        this.loaded$ = combineLatest([this.userService.user$]).pipe(take(1),
+            switchMap(([user]) => this.createVeriffForm(user))
         );
 
         this.finishedSubject = new Subject();
 
         this.finished$ = this.finishedSubject.asObservable().pipe(
-            switchMap(clientToken => onboardingService.verifyUser(clientToken.session_state)
-                .pipe(displayBackendErrorRx())),
-            switchMap(() => this.popupService.success('Successfully verified user data.')),
-            switchMap(() => this.loginService.refreshUserToken()
-                .pipe(displayBackendErrorRx())),
-            catchError(() => {
-                this.router.navigate(['/dash/settings/user']);
-                return EMPTY;
-            }),
-            switchMap(() => {
-                this.router.navigate(['/dash/wallet']);
-                return EMPTY;
-            })
+            switchMap(formResponse => this.createVeriffFrame(formResponse)),
+            switchMap(() => EMPTY)
         );
+
+        // this.finished$ = this.finishedSubject.asObservable().pipe(
+        //     switchMap(clientToken => onboardingService.verifyUser(clientToken.session_state)
+        //         .pipe(displayBackendErrorRx())),
+        //     switchMap(() => this.popupService.success('Successfully verified user data.')),
+        //     switchMap(() => this.loginService.refreshUserToken()
+        //         .pipe(displayBackendErrorRx())),
+        //     catchError(() => {
+        //         this.router.navigate(['/dash/settings/user']);
+        //         return EMPTY;
+        //     }),
+        //     switchMap(() => {
+        //         this.router.navigate(['/dash/wallet']);
+        //         return EMPTY;
+        //     })
+        // );
     }
 
-    private loadIdentyumScript(): Observable<void> {
+    private createVeriffForm(user: User): Observable<void> {
         return new Observable(subscriber => {
-            const script: HTMLScriptElement = this.renderer2.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://web-components.stage.identyum.com/flow-manager/component';
-            script.onload = () => {
-                subscriber.next();
-                subscriber.complete();
-            };
-            script.onerror = () => {
-                subscriber.error();
-                subscriber.complete();
-            };
+            const veriff = Veriff({
+                host: 'https://stationapi.veriff.com',
+                apiKey: 'a2b3005f-e324-4e7e-a328-4aecd11a091b',
+                parentId: 'veriff-root',
+                onSession: (err, response: VeriffFormResponse) => {
+                    if (err) {
+                        subscriber.error(err);
+                        subscriber.complete();
+                    } else {
+                        console.log(response);
+                        this.finishedSubject.next(response);
+                        subscriber.next();
+                        subscriber.complete();
+                    }
+                },
+            });
 
-            this.renderer2.appendChild(this.document.head, script);
+            veriff.setParams({
+                // TODO: uncomment this if not need user's name in the beginning of the flow
+                // person: {
+                //     givenName: ' ',
+                //     lastName: ' '
+                // },
+                vendorData: user.uuid
+            });
+
+            veriff.mount();
         });
     }
 
-    private setFlowManager(clientToken: IdentyumClientToken, startLanguage: string): Observable<void> {
+    private createVeriffFrame(formResponse: VeriffFormResponse): Observable<MESSAGES> {
         return new Observable(subscriber => {
-            const flowManager: any = document.createElement('idy-flow-manager');
-            flowManager.clientToken = clientToken;
-            flowManager.startLanguage = startLanguage;
+            const veriffFrame = createVeriffFrame({
+                url: formResponse.verification.url,
+                onEvent: function (msg) {
+                    switch (msg) {
+                        case MESSAGES.STARTED:
+                            console.log('started');
+                            break;
+                        case MESSAGES.FINISHED:
+                            console.log('finished');
+                            break;
+                        case MESSAGES.CANCELED:
+                            console.log('canceled');
+                            veriffFrame.close();
+                            break;
+                    }
 
-            flowManager.addEventListener('ready', _event => {
-                subscriber.next();
-                subscriber.complete();
+                    subscriber.next(msg);
+                    subscriber.complete();
+                }
             });
-            flowManager.addEventListener('finished', _event => {
-                this.finishedSubject.next(clientToken);
-            });
-
-            this.identyumContainer.nativeElement.appendChild(flowManager);
         });
     }
 }
 
-interface IdentyumClientToken {
-    access_token: string;
-    expires_in: number;
-    refresh_expires_in: number;
-    refresh_token: string;
-    session_state: string;
+interface VeriffFormResponse {
+    status: string;
+    verification: {
+        id: string;
+        url: string;
+        host: string;
+        status: string;
+        sessionToken: string;
+    };
 }

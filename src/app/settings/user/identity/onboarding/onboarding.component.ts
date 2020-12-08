5d785f6f1f17ffc9946021ad52551673db391d19
Matija Pevec <pevec@live.com>
2020-12-08 20:34:40 +0100

    new veriff logic

--- src/app/settings/user/identity/onboarding/onboarding.component.ts
+++ src/app/settings/user/identity/onboarding/onboarding.component.ts
@@ -1,16 +1,13 @@
-import { Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
-import { OnboardingService } from '../../../../shared/services/user/onboarding.service';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { DecisionStatus, OnboardingService, State, VeriffSession } from '../../../../shared/services/user/onboarding.service';
 import { UserAuthService } from '../../../../shared/services/user/user-auth.service';
-import { DOCUMENT } from '@angular/common';
-import { combineLatest, EMPTY, Observable, Subject } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, Observable, of, Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { PopupService } from '../../../../shared/services/popup.service';
 import { AppConfigService } from '../../../../shared/services/app-config.service';
 import { RouterService } from '../../../../shared/services/router.service';
 import { UserService } from '../../../../shared/services/user/user.service';
 import { createVeriffFrame, MESSAGES } from '@veriff/incontext-sdk';
-import { User } from '../../../../shared/services/user/signup.service';
-import Veriff from '@veriff/js-sdk';
 
 
 @Component({
@@ -21,26 +18,25 @@ import Veriff from '@veriff/js-sdk';
 export class OnboardingComponent {
     @ViewChild('identyumContainer') identyumContainer: ElementRef;
 
-    loaded$: Observable<void>;
+    session$: Observable<VeriffSession>;
+    private sessionSubject = new BehaviorSubject<VeriffSession>(null);
     finished$: Observable<void>;
     finishedSubject = new Subject<VeriffFormResponse>();
 
     constructor(private renderer2: Renderer2,
-                @Inject(DOCUMENT) private document: Document,
                 private appConfig: AppConfigService,
                 private router: RouterService,
                 private popupService: PopupService,
                 private userService: UserService,
                 private onboardingService: OnboardingService,
                 private loginService: UserAuthService) {
-        this.loaded$ = combineLatest([this.userService.user$]).pipe(take(1),
-            switchMap(([user]) => this.createVeriffForm(user))
+        this.session$ = this.sessionSubject.asObservable().pipe(
+            switchMap(session => session !== null ? of(session) : this.onboardingService.getVeriffSession())
         );
 
         this.finishedSubject = new Subject();
 
         this.finished$ = this.finishedSubject.asObservable().pipe(
-            switchMap(formResponse => this.createVeriffFrame(formResponse)),
             switchMap(() => EMPTY)
         );
 
@@ -61,61 +57,39 @@ export class OnboardingComponent {
         // );
     }
 
-    private createVeriffForm(user: User): Observable<void> {
-        return new Observable(subscriber => {
-            const veriff = Veriff({
-                host: 'https://stationapi.veriff.com',
-                apiKey: 'a2b3005f-e324-4e7e-a328-4aecd11a091b',
-                parentId: 'veriff-root',
-                onSession: (err, response: VeriffFormResponse) => {
-                    if (err) {
-                        subscriber.error(err);
-                        subscriber.complete();
-                    } else {
-                        console.log(response);
-                        this.finishedSubject.next(response);
-                        subscriber.next();
+    createVeriffFrame(verification_url: string): () => Observable<MESSAGES> {
+        return () => {
+            return new Observable(subscriber => {
+                const veriffFrame = createVeriffFrame({
+                    url: verification_url,
+                    onEvent: function (msg) {
+                        switch (msg) {
+                            case MESSAGES.STARTED:
+                                console.log('started');
+                                break;
+                            case MESSAGES.FINISHED:
+                                console.log('finished');
+                                break;
+                            case MESSAGES.CANCELED:
+                                console.log('canceled');
+                                veriffFrame.close();
+                                break;
+                        }
+
+                        subscriber.next(msg);
                         subscriber.complete();
                     }
-                },
+                });
             });
-
-            veriff.setParams({
-                // TODO: uncomment this if not need user's name in the beginning of the flow
-                // person: {
-                //     givenName: ' ',
-                //     lastName: ' '
-                // },
-                vendorData: user.uuid
-            });
-
-            veriff.mount();
-        });
+        };
     }
 
-    private createVeriffFrame(formResponse: VeriffFormResponse): Observable<MESSAGES> {
-        return new Observable(subscriber => {
-            const veriffFrame = createVeriffFrame({
-                url: formResponse.verification.url,
-                onEvent: function (msg) {
-                    switch (msg) {
-                        case MESSAGES.STARTED:
-                            console.log('started');
-                            break;
-                        case MESSAGES.FINISHED:
-                            console.log('finished');
-                            break;
-                        case MESSAGES.CANCELED:
-                            console.log('canceled');
-                            veriffFrame.close();
-                            break;
-                    }
+    showVerificationButton(session: VeriffSession) {
+        return session.state !== State.SUBMITTED || session.decision === null;
+    }
 
-                    subscriber.next(msg);
-                    subscriber.complete();
-                }
-            });
-        });
+    showDecision(session: VeriffSession) {
+        return !!session.decision.status;
     }
 }
 

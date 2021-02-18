10c2f075c7fc49d934de3612bdec9f433d377133
Matija Pevec <pevec@live.com>
2021-02-18 17:57:43 +0100

    --wip-- [skip ci]

---
+++ src/app/projects/projects-my/projects-my.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-my',
+  templateUrl: './projects-my.component.html',
+  styleUrls: ['./projects-my.component.scss']
+})
+export class ProjectsMyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/projects/projects.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectsMyComponent } from './projects-my/projects-my.component';
+
+
+
+@NgModule({
+  declarations: [ProjectsMyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProjectsModule { }
